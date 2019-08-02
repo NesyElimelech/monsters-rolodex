cc45096efd8bc659453c03aa06ea5c1176b65a4c
NesyElimelech <nesy15@gmail.com>
2019-08-02 14:02:52 +0300

    Updates

---
+++ precache-manifest.f96173ba62bbbe7133c889ff37d490f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba7705fce170b9f6f8a8a0a35e0a77a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0d244c18587666c4206",
+    "url": "/monsters-rolodex/static/css/main.c26a418f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0d244c18587666c4206",
+    "url": "/monsters-rolodex/static/js/main.94f3d184.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
